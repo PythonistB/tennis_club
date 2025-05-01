c88578201ff5874c9594d4c37b0b0fdb185bbe7b
Begnazar <99097880+PythonistB@users.noreply.github.com>
2025-05-01 14:44:49 +0300

    Add files via upload

---
+++ create_admin.py
@@ -0,0 +1,9 @@
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+if not User.objects.filter(username="admin").exists():
+    User.objects.create_superuser("Nazar", "nazar@mail.ru", "1234")
+    print("Superuser 'admin' created.")
+else:
+    print("Superuser 'admin' already exists.")
